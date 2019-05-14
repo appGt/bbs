9938203a0176c72f6de997d2b1422cc1d9f21daa
zzy <814793367@qq.com>
2019-05-15 01:06:07 +0800

    SSR base config

--- client/app.js
+++ client/app.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './app.jsx';
 
-ReactDom.render(<App/>, document.body)
+ReactDom.render(<App/>, document.getElementById('root'))
