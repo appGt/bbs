c0f107198ed7966f21e3deb12e9ec4597b35e622
zzy <814793367@qq.com>
2019-05-26 23:58:56 +0800

    目录架构

---
+++ client/views/topic-detail/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class TopicDetail extends React.Component {
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (
+      <div>This is topic list</div>
+    )
+  }
+}
